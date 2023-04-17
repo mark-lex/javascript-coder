1b86657ab9d5d308f4bdb4b86c77f9c4a266d586
Mark Moran <mark.alxndr8@gmail.com>
2023-04-16 22:18:25 -0500

    create login using while, if and else if

--- js/main.js
+++ js/main.js
@@ -1,19 +1,67 @@
 //! PRE ENTREGA 1
-//? CARRITO DE COMPRAS
+//? LOGIN
+let username;
+let password;
+let passLenght;
 
-let cart = " ";
-let answer = prompt("¿Quieres agregar un producto al carrito? (Sí/No)");
+while(!username || !password || !passLenght > 14){
+    username = prompt("Ingresa un nombre de usuario: ");
+    password = prompt("Ingresa una contraseña: ");
 
-if (answer.toLowerCase() === "sí") {
-    let product = prompt("Ingresa el nombre del producto:");
-    cart.push(product);
-}
+    passLenght=password.length;
 
-console.log("Carrito de compras:");
-if (cart.length === 0) {
-    console.log("El carrito está vacío");
-} else {
-    for (let i = 0; i < cart.length; i++) {
-        console.log("- " + cart[i]);
+    if (!username) {
+        alert("Debes ingresar un nombre de usuario: ");
+    } else if (!password){
+        alert("Debes ingresar una contraseña: ");
+    } else if (passLenght > 14){
+        alert("La contraseña no puede tener más de 14 caracteres")
     }
 }
+
+alert("Te damos la bienvenida a nuestra tienda: " + username);
+
+//? COMPRAR PRODUCTO
+/* function listProducts () {
+
+}
+
+let products = prompt("Escoge un producto: ") ;
+
+while(){
+
+    if (products = "") {
+        alert("El producto selecionado es: " + selectProduct)
+    } else {
+        alert("No seleccionaste ningún producto")
+    }
+
+} */
+
+
+//? CALCULANDO IGV - SUBTOTAL DE PRODUCTO
+/* function calcularIGV(price, igvPorcentaje) {
+    const IGV = price * igvPorcentaje;
+    return IGV;
+}
+
+const productPrice = Number(prompt("Ingrese el precio del producto: "));
+const igvPorcentaje = 0.18; //En Perú, el IGV es del 18%
+
+const igvCalculado = calcularIGV(productPrice, igvPorcentaje);
+const subtotal = productPrice - igvCalculado;
+
+console.log(`El IGV calculado es: ${igvCalculado.toFixed(2)}`);
+console.log(`El subtotal es: ${subtotal.toFixed(2)}`); */
+
+//!--------------------------------------------------------*
+// TEST
+/* function calcularIgv (productPrice)     {
+    const IGV_PORCENTAJE = 0.18;
+    const IGV = productPrice * IGV_PORCENTAJE;
+    return IGV;
+}
+
+const price = 35;
+const igvCalculado = calcularIgv (price);
+console.log("El IGV del producto es: " + igvCalculado); */
